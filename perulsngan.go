0d9a19103320e1ea763678539afae31e18b307db
golangiticm <golangiticm@gmail.com>
2024-02-05 02:12:01 +0700

    forloops

--- perulsngan.go
+++ perulsngan.go
@@ -10,32 +10,77 @@ func main() {
 		fmt.Println("Nilai i = ", i)
 	} //mencetak angka ber-urutan dari terkceil hingga maksimal nilai
 
-fmt.Println(strings.Repeat("=",10))
+	fmt.Println(strings.Repeat("=", 10))
 
-	for j := 5; j >-1; j-- {
+	for j := 5; j > -1; j-- {
 		fmt.Println("Nilai i = ", j)
 	} //mencetak angka ber-urutan dari terbesar hingga minimal nilai
 
-fmt.Println(strings.Repeat("=",10))
-  
+	fmt.Println(strings.Repeat("=", 10))
 
-var k, l int
+	var k, l int
 
-for k < 5{
-	fmt.Println("Angka ", k)
-	k++
-}
-
- //ini perulangan menggunakan argumen kondisi
-fmt.Println(strings.Repeat("=",13))
-
-for {
-	fmt.Println("angka", l)
-	l++
-
-	if l == 5 {
-		break
+	for k < 5 {
+		fmt.Println("Angka ", k)
+		k++
 	}
-} //sedangkan ini perulangan tanpa argumen 
 
+	//ini perulangan menggunakan argumen kondisi
+	fmt.Println(strings.Repeat("=", 13))
+
+	for {
+		fmt.Println("angka", l)
+		l++
+
+		if l == 5 {
+			break
+		}
+	} //sedangkan ini perulangan tanpa argumen
+
+	for i := 0; i < 5; i++ {
+
+	}
+
+	fmt.Println(strings.Repeat("=", 13))
+
+	var xs = "012345678"
+
+	for z, v := range xs {
+		fmt.Println("index", z, "Value", v)
+	} //pengecekan nilai ASCII dimana 0 itu = 48 byte
+
+	for a := 0; a < 5; a++ {
+		for b := a; b < 5; b++ {
+			fmt.Print(b, " ")
+		}
+		fmt.Println()
+	}
+
+	fmt.Println(strings.Repeat("=", 13))
+
+	for f := 0; f <= 10; f++ {
+		if f%2 == 0 {
+			fmt.Println("Angka : ", f)
+		}
+	} // mencetak angka genap
+
+
+	
+	fmt.Println(strings.Repeat("=", 13))
+
+	for g := 0; g <= 10; g++ {
+		if g%2 == 1 {
+			fmt.Println("Angka : ", g)
+		}
+	} // mencetak angka ganjil
+
+
+	
+	fmt.Println(strings.Repeat("=", 13))
+
+	for g := 10; g >0; g-- {
+		if g%2 == 1 {
+			fmt.Println("Angka : ", g)
+		}
+	} // mencetak angka ganjil
 }
